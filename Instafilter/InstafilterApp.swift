bf267065e50784d758ff1ce7409f34ee21f41b5d
Alex Zhulai <Lepiozdyx@gmail.com>
2023-12-21 11:05:47 -0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Alex on 21.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
